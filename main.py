6509ba6cd0a5ae3793c06d5ac636cc86f920e496
jameswiise <jamescarr92@gmail.com>
2025-03-10 10:04:30 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+def main ():
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen width: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+main()
