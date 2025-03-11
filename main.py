f60578a6c5ea2c73fbae357ab6c0a349256f0af5
jameswiise <jamescarr92@gmail.com>
2025-03-11 19:52:49 +1100

    created a game loop

--- main.py
+++ main.py
@@ -2,13 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
+
 from constants import *
 def main ():
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen width: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    number = 5
+    while number != 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(black)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
